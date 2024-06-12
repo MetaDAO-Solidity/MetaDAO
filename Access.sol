7ab98cf8be2e40d472152c651b30226a7f6beb54
MetaDAO-Solidity <metadaolabs@gmail.com>
2024-06-12 12:45:38 +0800

    Update Access.sol

--- Access.sol
+++ Access.sol
@@ -1,1 +1,17 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+import "@openzeppelin/contracts@4.9.0/access/AccessControlEnumerable.sol";
+
+contract Access is AccessControlEnumerable {
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  constructor(address _gov) {
+    _grantRole(DEFAULT_ADMIN_ROLE, _gov);
+  }
+
+  modifier onlyGovernance() {
+    require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "VM: Not governance");
+    _;
+  }
+}
